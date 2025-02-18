ba0d6239a9c8ee7ab016d5d239bcf426cf48c9af
faratfan <164708808+faratfan@users.noreply.github.com>
2025-02-18 14:46:29 +0000

    if statement

--- Assignment/project_01.js
+++ Assignment/project_01.js
@@ -1,1 +1,26 @@
 console.log("Working on FirstProject");
+const time= 12;
+const time2= "1pm";
+const time3="4pm";
+
+
+//if statement
+if(time < 12){
+   console.log("Good Morring")
+}else if(time => 12){
+  console.log("Good Afternoon")
+}else{
+    console.log("Good Evening")
+}
+// switch/case
+switch(time2){
+    case "1pm":
+        console.log("Good Afternoon");
+        break;
+    case "4pm":
+        console.log("Good Evening");
+        break;
+    default:
+        console.log("Good Night");
+        break;
+}
